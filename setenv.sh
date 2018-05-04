3c0721e7a0281b82752af78f9ff505e844ba93bc
Julia Partridge <julia.partridge@aqauq.co.uk>
2018-05-04 14:25:42 +0100

    #88 Update environment variables

--- setenv.sh
+++ setenv.sh
@@ -1,17 +1,17 @@
 export TORQHOME=${PWD}                                                                              # if running the kdb+tick example, change these to full paths
 export TORQDATA=${PWD}                                                                              # some of the kdb+tick processes will change directory, and these will no longer be valid
 export TORQAPPHOME=${PWD}
+
 export KDBLOG=${TORQDATA}/logs
 export KDBHTML=${TORQHOME}/html
 export KDBLIB=${TORQHOME}/lib
+export KDBCONFIG=${TORQHOME}/config
+export KDBCODE=${TORQHOME}/code
+export KDBAPPCONFIG=${TORQAPPHOME}/appconfig                                                        # sets the application specific configuration directory
 export KDBAPPCODE=${TORQAPPHOME}/code
-export KDBCONFIG=${TORQAPPHOME}/config
-export KDBCODE=${TORQAPPHOME}/code
 export KDBHDB=${TORQDATA}/hdb/database
 export KDBWDB=${TORQDATA}/wdbhdb
 
-
-export KDBAPPCONFIG=${TORQHOME}/appconfig                                                           # sets the application specific configuration directory
 export KDBBASEPORT=41000                                                                            # set KDBBASEPORT to the default value for a TorQ Installation
 export KDBSTACKID="-stackid ${KDBBASEPORT}"
 export DEFAULTCSV=${KDBAPPCONFIG}/process.csv                                                       # set DEFAULTCSV to the default process csv
