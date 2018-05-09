5f261a84bbec082a7081ca58d34119ba0443d5a2
Julia Partridge <julia.partridge@aqauq.co.uk>
2018-05-09 17:05:06 +0100

    #88 Remove ssl certification

--- setenv.sh
+++ setenv.sh
@@ -12,20 +12,8 @@ export KDBAPPCODE=${TORQAPPHOME}/code
 export KDBHDB=${TORQDATA}/hdb/database
 export KDBWDB=${TORQDATA}/wdbhdb
 
-export KDBBASEPORT=41000                                                                            # set KDBBASEPORT to the default value for a TorQ Installation
+export KDBBASEPORT=17000                                                                            # set KDBBASEPORT to the default value for a TorQ Installation
 export KDBSTACKID="-stackid ${KDBBASEPORT}"
-export DEFAULTCSV=${KDBAPPCONFIG}/process.csv                                                       # set DEFAULTCSV to the default process csv
+export TORQPROCESSES=${KDBAPPCONFIG}/process.csv                                                       # set DEFAULTCSV to the default process csv
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$KDBLIB/l32
-
-
-touch $KDBLOG/torqsslcert.txt
-if [ -z "${SSL_CA_CERT_FILE}" ]; then
-	mkdir ${PWD}/certs
-	curl -s  https://curl.haxx.se/ca/cacert.pm > ${PWD}/certs/cabundle.pem
-	echo "`date`    The SSL securiity certificate has been downloaded to ${PWD}/certs/cabundle.pem" </dev/null >>$KDBLOG/torqsslcert.txt 
-	export SSL_CA_CERT_FILE=${PWD}/certs/cabundle.pem
-else
-	echo "`date`    The SSL security certificate already exists. If https requests fail it may be because of inappropriate certification." </dev/null >>$KDBLOG/torqsslcert.txt 
-fi
-
