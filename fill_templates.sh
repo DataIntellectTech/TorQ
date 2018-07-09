e95611d16205a57f6f464f7c06ec7fe9e7595633
Aqualex <rdanutalexandru@gmail.com>
2018-07-09 11:56:03 +0100

    Added function createmonitrc which takes the procname and proctype from process.csv Added monitconfig.cfg which contains the location of the process.csv and the output file

--- fill_templates.sh
+++ fill_templates.sh
@@ -3,20 +3,35 @@
 #FUNTION DECLARATION 
 createmonitrc(){
   #function to read all processes from the processes.csv 
-  echo $1 
-  echo $2 
-  echo $3
+  #and build the array
+  
+  cat $1|while read line; do
+  conf=($line)
+  procs=$(eval "echo \"${conf[0]}\"")
+  #echo $procs
+  output="$configs/${conf[1]}"
+  done
+
+  proclist=`tail -n +2 /home/$USER/TorQProdSupp/TorQDev/TorQ-Finance-Starter-Pack/appconfig/process.csv | awk -F "\"*,\"*" '{print  $3 " " $4}'|cut -d" " -f1,2`
+  echo "$proclist"|while read procs; do 
+    array=($procs)
+    proctype=${array[0]}
+    procname=${array[1]}
+    "eval echo $2"
+    #eval "{$2}" >> test.txt
+  done 
+
 }
 
-createoutput(){
+#createoutput(){
   #function to create the output file 
 
-}
+#}
 
-createmonitrc(){
+#createmonitrc(){
   #function to create the monitrc file
 
-}
+#}
 
 #SETTING ENVIRONMENT VARIABLES
 if [ "-bash" = $0 ]; then
@@ -27,14 +42,17 @@ fi
 
 eval ". $(dirname "$dirpath")/setenv.sh"                                        #set environment variables  
 templates="${TORQHOME}/code/monit/templates"                                    #set temmplates folder 
-config="${TORQHOME}/code/monit/"                                               #set configs folder 
+config="${TORQHOME}/code/monit/"                                                #set configs folder 
 monit_control="${TORQHOME}/config/monitrc"                                      #set output file for main monit conf
 
 mkdir -p $config                                                                #ensure output directory is created
 
-#monittemplate="$(cat $templates/monittemplate.txt)"
+monittemplate="$(cat $templates/monittemplate.txt)"
 
-createmonitrc "$templates/monitconfig.cfg" "one" "two" 
+#echo ${monittemplate}
+
+createmonitrc "$templates/monitconfig.cfg" "$monittemplate"
 
 #TESTING (to be removed)
 #echo ${TORQHOME}
+#echo $templates
