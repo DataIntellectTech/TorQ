2dcc6c1547fe3ac421ee9cd35caafa6196d101db
Aqualex <rdanutalexandru@gmail.com>
2018-08-08 17:12:16 +0100

    Modified setenv.sh to set the path for Torq direcotry based on the directory the script is executed from

--- setenv.sh
+++ setenv.sh
@@ -1,6 +1,12 @@
-export TORQHOME=${PWD}                                                                   # if running the kdb+tick example, change these to full paths
-export TORQDATA=${PWD}                                                                            # some of the kdb+tick processes will change directory, and these will no longer be valid
-export TORQAPPHOME=${PWD}
+if [ "-bash" = $0 ]; then
+    dirpath="${BASH_SOURCE[0]}"
+else
+    dirpath="$0"
+fi
+
+export TORQHOME=$(dirname $dirpath)                                                                 # if running the kdb+tick example, change these to full paths
+export TORQDATA=$(dirname $dirpath)                                                                 # some of the kdb+tick processes will change directory, and these will no longer be valid
+export TORQAPPHOME=$(dirname $dirpath)
 
 export KDBLOG=${TORQDATA}/logs
 export KDBHTML=${TORQHOME}/html
