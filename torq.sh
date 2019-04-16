174cde2ff877d80b1c6866a68cb5222da09a2e10
Vincent Bernardoff <vb@luminar.eu.org>
2019-04-16 18:07:01 +0200

    Add default for qcmd if not defined (#149)
    * Add default q command if unspecified
    
    * Add comment to address undefined qcmd action

--- torq.sh
+++ torq.sh
@@ -59,6 +59,9 @@ startline() {
     sline="$sline$a";                                                                               # append to startup line
   done
   qcmd=$(getfield "$procno" "qcmd")
+  if [ -z $qcmd ]; then                                                                             # if qcmd is undefined then default to q
+    qcmd="q"
+  fi
   sline="$qcmd $sline $(getfield "$procno" extras) -procfile $CSVPATH $EXTRAS"                      # append csv file and extra arguments to startup line
   echo "$sline"
  }
