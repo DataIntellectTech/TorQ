ad40ab4d32d38e834abf6bdddf41ddb28ce4a2d8
PeterSmiley <petersmiley@hotmail.co.uk>
2019-11-07 11:35:08 +0000

    Comments added to sendto files

--- datadog/sendEventToDatadog.sh
+++ datadog/sendEventToDatadog.sh
@@ -2,5 +2,5 @@
 
 event_title=$1
 event_text=$2
-
-echo "_e{${#event_title},${#event_text}}:$event_title|$event_text|#shell,bash" >/dev/udp/127.0.0.1/8125
+#Length of event title,length of event text,the event title,the event text with commentt shell,bash are sent to datadog on port 8125.
+echo "_e{${#event_title},${#event_text}}:$event_title|$event_text|#shell,bash">/dev/udp/127.0.0.1/8125
--- datadog/sendToDatadog.sh
+++ datadog/sendToDatadog.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo -n "$2:$1|g|#shell" >/dev/udp/127.0.0.1/8125
-
+#send metric name, value to datadog 
