15ef9c6f911d8c9310d941caeb6a98efef9ef9fe
PeterSmiley <petersmiley@hotmail.co.uk>
2019-11-07 11:19:37 +0000

    Correct location for file

--- datadog/createdatadogyaml.sh
+++ datadog/createdatadogyaml.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+#Filename is set to be in correct directory
 filename=/etc/datadog-agent/datadog.yaml
-#filename=~/datadog.yaml
+
+#If the file doesn't exist, make file and remove restrictions. Add the "echo" lines and save to file.
 if [[ ! -f $filename ]];then
  sudo touch $filename
  sudo chmod 777 $filename
@@ -13,3 +15,4 @@ dogstatsd_port: 8125
 process_config:
  enabled: \"true\"" >> $filename
 fi
+#Need to be able to find api_key somehow, or enter it here manually.
