d218ffe796efb9fb8bb0efd699949cff44bd0b9e
PeterSmiley <petersmiley@hotmail.co.uk>
2019-11-20 14:06:42 +0000

    Removed If loop

--- datadog/createdatadogyaml.sh
+++ datadog/createdatadogyaml.sh
@@ -3,6 +3,7 @@
 #Filename is set to be in correct directory
 filename=/etc/datadog-agent/datadog.yaml
 
+
 #Remove file restrictions add the "echo" lines and save to file.
 sudo chmod 777 $filename
 sudo echo "use_dogstatsd: true
