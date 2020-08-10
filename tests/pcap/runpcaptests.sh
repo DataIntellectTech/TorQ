caa92350a3dda676682c6208aae98f040b1d78c4
Andrew West <andrew.west@aquaq.co.uk>
2020-08-10 15:34:01 +0100

    Remove hard coded rlwrap path

--- tests/pcap/runpcaptests.sh
+++ tests/pcap/runpcaptests.sh
@@ -3,5 +3,5 @@
 ./torq.sh start discovery1
 ./torq.sh start tickerplant1 
 ./torq.sh start rdb1 -extras -.rdb.replaylog 0
-/usr/bin/rlwrap q torq.q -load ${KDBCODE}/processes/filealerter.q -proctype filealerter -procname filealerter1 -test ${KDBTESTS}/pcap -debug -.fa.decodepcaps 1 -.fa.polltime 00:10 -.servers.CONNECTIONS rdb -.fa.alreadyprocessed ${KDBTESTS}/pcap/filealerterprocessed -.fa.inputcsv ${KDBTESTS}/pcap/filealerter.csv
+rlwrap q torq.q -load ${KDBCODE}/processes/filealerter.q -proctype filealerter -procname filealerter1 -test ${KDBTESTS}/pcap -debug -.fa.decodepcaps 1 -.fa.polltime 00:10 -.servers.CONNECTIONS rdb -.fa.alreadyprocessed ${KDBTESTS}/pcap/filealerterprocessed -.fa.inputcsv ${KDBTESTS}/pcap/filealerter.csv
 ./torq.sh stop all
