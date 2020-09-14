1dd3089cba04b7370b752fc8e1af95781929081e
Michael <michael.potter@aquaq.co.uk>
2020-09-14 17:41:42 +0100

    Tidy up subscription file test start script

--- tests/stp/subfile/runsubfiletests.sh
+++ tests/stp/subfile/runsubfiletests.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-./torq.sh start discovery1
-./torq.sh start tickerplant1
-./torq.sh start stp1
-./torq.sh start rdb1
+./torq.sh start discovery1 tickerplant1 stp1 rdb1
 /usr/bin/rlwrap q torq.q -proctype test -procname test1 -test ${KDBTESTS}/stp/subfile/ -debug
-./torq.sh stop all
+./torq.sh stop discovery1 tickerplant1 stp1 rdb1
