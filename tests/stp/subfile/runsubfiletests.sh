1d0a7a126d463c84be455c065888f0a6fd0a1806
Michael <michael.potter@aquaq.co.uk>
2020-09-15 09:35:10 +0100

    More test script tidying

--- tests/stp/subfile/runsubfiletests.sh
+++ tests/stp/subfile/runsubfiletests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-./torq.sh start discovery1 tickerplant1 stp1 rdb1
-/usr/bin/rlwrap q torq.q -proctype test -procname test1 -test ${KDBTESTS}/stp/subfile/ -debug
-./torq.sh stop discovery1 tickerplant1 stp1 rdb1
+${TORQHOME}/torq.sh start discovery1 tickerplant1 stp1 rdb1
+/usr/bin/rlwrap q ${TORQHOME}/torq.q -proctype test -procname test1 -test ${KDBTESTS}/stp/subfile/ -debug
+${TORQHOME}/torq.sh stop discovery1 tickerplant1 stp1 rdb1
--- tests/stp/subscription/runSubTests.sh
+++ tests/stp/subscription/runSubTests.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-nohup q ${KDBTESTS}/stp/general/dummyclient.q -schemafile ${TORQHOME}/database.q -p $((${KDBBASEPORT}+100)) &
-/usr/bin/rlwrap q torq.q -load ${KDBCODE}/processes/segmentedtickerplant.q -schemafile ${TORQHOME}/database.q -proctype segmentedtickerplant -procname stp1 -test ${KDBTESTS}/stp/subscription -debug
+nohup q ${KDBTESTS}/stp/subscription/dummyclient.q -schemafile ${TORQHOME}/database.q -p $((${KDBBASEPORT}+100)) &
+/usr/bin/rlwrap q ${TORQHOME}/torq.q -load ${KDBCODE}/processes/segmentedtickerplant.q -schemafile ${TORQHOME}/database.q -proctype segmentedtickerplant -procname stp1 -test ${KDBTESTS}/stp/subscription -debug
