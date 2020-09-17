beb4bad435d096b17ab75923b73efdc18300d000
Michael <michael.potter@aquaq.co.uk>
2020-09-17 12:50:20 +0100

    Add 2.3.4 and tweaks to 1.1.3, 1.1.4

--- tests/stp/recovery/runstprecoverytests.sh
+++ tests/stp/recovery/runstprecoverytests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-./torq.sh start discovery1 -csv ${KDBTESTS}/stp/recovery/process.csv
-/usr/bin/rlwrap q torq.q -proctype test -procname test1 -test ${KDBTESTS}/stp/recovery -debug
-./torq.sh stop discovery1 -csv ${KDBTESTS}/stp/recovery/process.csv
+${TORQHOME}/torq.sh start discovery1 -csv ${KDBTESTS}/stp/recovery/process.csv
+/usr/bin/rlwrap q ${TORQHOME}/torq.q -proctype test -procname test1 -test ${KDBTESTS}/stp/recovery -debug
+${TORQHOME}/torq.sh stop discovery1 -csv ${KDBTESTS}/stp/recovery/process.csv
