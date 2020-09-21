09c982322d4a9d3a871d7bad4b56fb542224b4dc
Michael <michael.potter@aquaq.co.uk>
2020-09-21 15:24:48 +0100

    Add vanilla TP tests (2.4.x)

---
+++ tests/stp/tpvalidation/run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Start procs
+${TORQHOME}/torq.sh start discovery1 tp1 rdball rdbsymfilt -csv ${KDBTESTS}/stp/tpvalidation/process.csv
+
+# Start test proc
+/usr/bin/rlwrap q ${TORQHOME}/torq.q \
+  -proctype test -procname test1 \
+  -test ${KDBTESTS}/stp/tpvalidation -debug \
+  -load ${KDBTESTS}/helperfunctions.q ${KDBTESTS}/stp/tpvalidation/settings.q \
+  -procfile ${KDBTESTS}/stp/tpvalidation/process.csv
+
+# Shut down procs
+${TORQHOME}/torq.sh stop discovery1 tp1 rdball rdbsymfilt -csv ${KDBTESTS}/stp/tpvalidation/process.csv
