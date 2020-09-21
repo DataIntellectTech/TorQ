9c30570426738ac680f2201d16b1853a403617b3
Michael <michael.potter@aquaq.co.uk>
2020-09-21 16:05:08 +0100

    Standardise folder structure and test run script names

---
+++ tests/stp/recovery/run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Start procs
+${TORQHOME}/torq.sh start discovery1 -csv ${KDBTESTS}/stp/recovery/process.csv
+
+# Start test proc
+/usr/bin/rlwrap q ${TORQHOME}/torq.q \
+  -proctype test -procname test1 \
+  -test ${KDBTESTS}/stp/recovery -debug \
+  -load ${KDBTESTS}/helperfunctions.q ${KDBTESTS}/stp/recovery/settings.q \
+  -procfile ${KDBTESTS}/stp/recovery/process.csv
+
+# Shut down procs
+${TORQHOME}/torq.sh stop discovery1 -csv ${KDBTESTS}/stp/recovery/process.csv
