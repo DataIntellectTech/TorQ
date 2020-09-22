1eee5f739c59e7bc45899ab97339048247259e4c
Michael <michael.potter@aquaq.co.uk>
2020-09-22 10:30:11 +0100

    Delete old test runner scripts

--- tests/stp/subscription/runSubTests.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-nohup q ${KDBTESTS}/stp/subscription/dummyclient.q -schemafile ${TORQHOME}/database.q -p $((${KDBBASEPORT}+100)) &
-/usr/bin/rlwrap q ${TORQHOME}/torq.q -load ${KDBCODE}/processes/segmentedtickerplant.q -schemafile ${TORQHOME}/database.q -proctype segmentedtickerplant -procname stp1 -test ${KDBTESTS}/stp/subscription -debug
-pkill -f dummyclient
