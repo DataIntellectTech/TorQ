8c07ac9e3f0cc29ec4dbef61aec12b4125bd6680
JCarton94 <jamie.carton@aquaq.co.uk>
2020-10-07 20:36:53 +0100

    Improve run.sh and add settings.q folder to tickerlog tests.

--- tests/stp/tickerlog/run.sh
+++ tests/stp/tickerlog/run.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 # This script runs tests 
 
-q torq.q -load $KDBCODE/processes/tickerlogreplay.q -.replay.tplogfile $KDBTESTS/stp/tickerlog/testlogs/testoldlog -proctype tickerlogreplay -procname tickerlogreplay1 -procfile ${KDBAPPCONFIG}/process.csv -localtime -.replay.schemafile database.q -.replay.hdbdir $KDBTESTS/stp/tickerlog/testhdb/ -.replay.segmentedmode 0 -.replay.exitwhencomplete 0 -test $KDBTESTS/stp/tickerlog/ -debug
+q torq.q \
+	-load $KDBCODE/processes/tickerlogreplay.q $KDBTESTS/stp/tickerlog/settings.q \
+	-.replay.tplogfile $KDBTESTS/stp/tickerlog/testlogs/testoldlog \
+	-proctype tickerlogreplay -procname tickerlogreplay1 \
+	-procfile ${KDBAPPCONFIG}/process.csv \
+	-localtime \
+	-.replay.schemafile database.q \
+	-.replay.hdbdir $KDBTESTS/stp/tickerlog/testhdb/ \
+	-.replay.exitwhencomplete 0 \
+	-.replay.segmentedmode 0 \
+	-test $KDBTESTS/stp/tickerlog/ \
+	-debug
 
