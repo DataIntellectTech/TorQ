2a3a28644648f4a0e57e648ea23f066ee8808506
etennoQ <elliot.tennison@aquaq.co.uk>
2020-10-16 14:40:09 +0100

    added test 8.2.2

---
+++ tests/stp/chainedrecovery/run.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Parse command line arguments -d:debug mode, -s:stop/debug mode, -q:quiet mode, -r:runtime
+# If no flags are passed custom logs will be generated in runtests.q
+while getopts ":dsqr:" opt; do
+  case $opt in
+    d ) debug="-debug" ;;
+    s ) debug="-debug";stop="-stop" ;;
+    q ) quiet="-q" ;;
+    r ) run=$OPTARG ;;
+    \?) echo "Usage: run.sh [-d] [-s] [-q] [-r runtimestamp]" && exit 1 ;;
+    : ) echo "$OPTARG requires an argument" && exit 1 ;;
+  esac
+done
+
+# Path to test directory
+testpath=${KDBTESTS}/stp/chainedrecovery
+
+# Start procs
+${TORQHOME}/torq.sh start discovery1 -csv ${testpath}/process.csv
+
+# Start test proc
+/usr/bin/rlwrap q ${TORQHOME}/torq.q \
+  -proctype test -procname test1 \
+  -test ${testpath} \
+  -load ${KDBTESTS}/helperfunctions.q ${testpath}/settings.q \
+  -results ${KDBTESTS}/stp/results/ \
+  -runtime $run \
+  -procfile ${testpath}/process.csv \
+  $debug $stop $quiet
+
+# Shut down procs
+${TORQHOME}/torq.sh stop discovery1 rdball stprepperiod stp1 stptabperiod rdb1 -csv ${testpath}/process.csv
