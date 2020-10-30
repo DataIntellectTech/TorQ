eeb7e3489cf2e8fb647cfa32a0d10b6bcd4b5abf
Michael <michael.potter@aquaq.co.uk>
2020-10-30 11:38:56 +0000

    Revamp housekeeping tests

--- tests/stp/housekeeping/run.sh
+++ tests/stp/housekeeping/run.sh
@@ -1,10 +1,22 @@
-# housekeeping
-cp -r $KDBTESTS/stp/housekeeping/testlog/teststpnone $KDBTESTS/stp/housekeeping/testlog/testlog
+#!/bin/bash
 
-q $TORQHOME/torq.q \
-        -load $KDBCODE/processes/housekeeping.q $KDBTESTS/stp/housekeeping/settings.q \
-        -proctype tickerlogreplay -procname tickerlogreplay1 \
-        -procfile $KDBAPPCONFIG/process.csv \
-        -localtime \
-        -test $KDBTESTS/stp/housekeeping/ \
-        -debug
+# Handle command-line arguments
+source $KDBTESTS/flagparse.sh
+
+# Path to test directory
+testpath=${KDBTESTS}/stp/housekeeping
+
+# Start procs
+${TORQHOME}/torq.sh start discovery1 housekeeping1
+
+# Start test proc
+/usr/bin/rlwrap q ${TORQHOME}/torq.q \
+  -proctype test -procname test1 \
+  -test ${testpath} \
+  -load ${KDBTESTS}/helperfunctions.q ${testpath}/settings.q \
+  -testresults ${KDBTESTS}/stp/results/ \
+  -runtime $run \
+  $debug $stop $write $quiet
+
+# Shut down procs
+${TORQHOME}/torq.sh stop discovery1 housekeeping1
