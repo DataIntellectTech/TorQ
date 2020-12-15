95da2ad1ddb954665d03374a05b4346d2b7a1e03
Luke Holmes <luke.holmes@aquaq.co.uk>
2020-12-15 10:40:11 +0000

    Add discovery startup back to tests

--- tests/dataaccess/checkinputs/run.sh
+++ tests/dataaccess/checkinputs/run.sh
@@ -6,6 +6,9 @@ source $KDBTESTS/flagparse.sh
 # Path to test directory
 testpath=${KDBTESTS}/dataaccess/checkinputs
 
+# Start procs
+${TORQHOME}/torq.sh start discovery1
+
 # Start test proc
 /usr/bin/rlwrap q ${TORQHOME}/torq.q \
   -proctype rdb -procname dailyrdb1 \
