d3def90b6b6ae5dd207cf63bd851b6580e7dc569
dan-sh <dan.sheppard@aquaq.co.uk>
2020-12-16 14:41:09 +0000

    removed some binaries

--- tests/dataaccess/queryorder/inputs/dictionarycleaner.sh
+++
@@ -1,7 +0,0 @@
-sed -i -z 's/tablename/parameter,parametervalue\ntablename/g' $1
-
-sed -i -z 's/,/enlist /g' $1
-
-sed -i -z 's/| /,/g' $1
-
-cat $1
--- tests/dataaccess/queryorder/outputs/dictionarycleaner.sh
+++
@@ -1,7 +0,0 @@
-sed -i -z 's/0|/parameter,parametervalue\n0|/g' $1
-
-sed -i -z 's/,/enlist /g' $1
-
-sed -i -z 's/| /,/g' $1
-
-cat $1
