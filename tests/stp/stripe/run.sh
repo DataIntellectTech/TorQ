a4c91cffaf876506956bc17c2af328472bbf0180
Maurice Lim <mauricelim@MAURICELIM.localdomain>
2021-11-17 15:58:25 +0800

    Add unit and performance tests for striping function

---
+++ tests/stp/stripe/run.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+cd $HOME/TorQ/deploy/TorQ/latest
+source setenv.sh
+
+# Handle command-line arguments
+source $KDBTESTS/flagparse.sh
+
+# Path to test directory
+testpath=${KDBTESTS}/stp/stripe
+export perfpath=${testpath}/performance
+
+# Start procs
+${TORQHOME}/torq.sh start discovery1 stp1 rdb1 rdb2 rdb3 rdb4 -csv ${testpath}/process.csv
+
+# Start test proc
+QCMD='taskset -c 0,1 /usr/bin/rlwrap q'
+${QCMD} ${TORQHOME}/torq.q \
+  -proctype test -procname test1 \
+  -test ${testpath} \
+  -load ${KDBTESTS}/helperfunctions.q ${testpath}/settings.q \
+  -testresults ${KDBTESTS}/stp/results/stripe/ \
+  -runtime $run \
+  -procfile ${testpath}/process.csv \
+  -write
+  #$debug $stop $write $quiet
+
+# k4unit test
+k4unit=$(find "$(cd ..; pwd)" -name "k4unit.q")
+# Performance test from stp
+cd $perfpath
+# Specify session name
+session=kdbProcess
+# Kill remaining session first
+tmux kill-session -t $session
+# Start a new session (kdbProcess) detached (-d)
+tmux new-session -d -s $session
+# Wait till session and window is created
+while ! tmux has-session -t $session:bash; do sleep 1; done
+# qcon into stp proc
+stpport=$(cat ${testpath}/process.csv | grep segmentedtickerplant | awk -F',' '{print $2}')
+tmux send-keys -t kdbProcess:bash -l qcon=\'/usr/bin/rlwrap\ /opt/kdb/qcon\'
+tmux send-keys -t kdbProcess:bash Enter
+tmux send-keys -t kdbProcess:bash stpport=\'$stpport\' Enter
+tmux send-keys -t kdbProcess:bash -l '$qcon homer.aquaq.co.uk:$stpport:admin:admin'
+tmux send-keys -t kdbProcess:bash Enter
+# Load the script file
+tmux send-keys -t kdbProcess:bash \\l\ $k4unit Enter
+# Load settings
+settings=$testpath/settings.q
+tmux send-keys -t kdbProcess:bash \\l\ $settings Enter
+# Run performance test and save results
+tmux send-keys -t kdbProcess:bash \\l\ $perfpath/run_perf_test.q Enter
+# Wait for test to complete
+tmux send-keys -t kdbProcess:bash \`char\$107\ 52\ 117\ 110\ 105\ 116\ 32\ 84\ 101\ 115\ 116\ 32\ 82\ 101\ 115\ 117\ 108\ 116\ 115i Enter
+tmux capture-pane -t kdbProcess
+done=$(tmux show-buffer | grep "k4unit Test Results" | wc -l)
+while ! [ $done -eq 1 ]; do
+    sleep 3
+    tmux capture-pane -t kdbProcess
+    echo k4unit Test is running...
+    done=$(tmux show-buffer | grep "k4unit Test Results" | wc -l)
+done
+# Kill tmux session
+tmux kill-session -t $session
+echo k4unit Test complete
+
+# Shut down procs
+${TORQHOME}/torq.sh stop discovery1 stp1 rdb1 rdb2 rdb3 rdb4 -csv ${testpath}/process.csv
---
+++ tests/stp/stripe/settings.q
@@ -0,0 +1,46 @@
+// IPC connection parameters
+.servers.CONNECTIONS:`rdb`segmentedtickerplant;
+.servers.USERPASS:`admin:admin;
+
+// Test trade batches
+syms:`${x,raze x,/:\: .Q.A}/[1;raze(raze .Q.A ,/:\: .Q.A),/:\: .Q.A];
+ls:raze(ls2:til 500)+/:0.01*til 101;
+src:`BARX`GETGO`SUN`DB;
+
+q:{[syms;len;ls;ls2;src] len?/:(syms;ls;ls;ls2;ls2;" 89ABCEGJKLNOPRTWZ";"NO";src)}[syms;;ls;ls2;src];
+t:{[syms;len;ls;ls2;src] len?/:(syms;ls;`int$ls2;01b;" 89ABCEGJKLNOPRTWZ";"NO";`buy`sell)}[syms;;ls;ls2;src];
+
+// Local trade table schema
+trade:flip `time`sym`price`size`stop`cond`ex`side!"PSFIBCCS" $\: ();
+quote:flip `time`sym`bid`ask`bsize`asize`mode`ex`src!"PSFFJJCCS" $\: ();
+
+// Striping function
+lookup:{[numSeg;maxProc]
+    hexDg:lower .Q.nA til maxProc;
+    seg:til numSeg;
+    hexDg!maxProc#til numSeg
+    }[;16];
+modMd5:{first each string first each md5'[string x]};
+map:{[modMd5;lookup;numSeg;sym] sym@/:group lookup[numSeg]modMd5 sym}[modMd5;lookup];
+stripe:{[map;numSeg;input]
+    $[numSeg within(1;16);;'"Max number of processes is 16"];
+    sym:distinct input;
+    $[`subReq in key`.;
+        [$[numSeg=1+max key subReq;;`subReq set ()!()];
+        $[any new:not sym in raze value subReq;
+            `subReq set subReq,'map[numSeg;sym where new];
+            ];];
+        `subReq set map[numSeg;sym]
+        ];}[map];
+// Get the number of rdb processes from process.csv
+numSeg:sum `rdb=((.proc`readprocs).proc`file)`proctype;
+skey:til numSeg;
+// Initialize subscription request for all syms to test striping function in stp
+stripe[numSeg;syms];
+
+// Local upd and error log function
+upd:{[t;x] t insert x};
+upderr:{[t;x].tst.err:x};
+
+// Test db name
+testlogdb:"testlog";
