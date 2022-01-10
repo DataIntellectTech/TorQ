b7831f0669ef549a594938762aa806d73f8efee7
cdempsey99 <callum.dempsey@aquaq.co.uk>
2022-01-10 16:51:42 +0000

    Improved method of finding process port in unit tests

--- tests/cloudlaunchprocess/run.sh
+++ tests/cloudlaunchprocess/run.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 #path to test directory
-testpath=${KDBTESTS}/lproctests
+testpath=${KDBTESTS}/cloudlaunchprocess
 
 #start procs
-${TORQHOME}/torq.sh start all -csv ${testpath}/lprocprocess.csv
+${TORQHOME}/torq.sh start all -csv ${testpath}/process.csv
 
 #Start test proc
 /usr/bin/rlwrap q ${TORQHOME}/torq.q \
-	-proctype test -procname hdb1 \
+	-proctype test -procname rdb1 \
 	-test ${testpath} \
 	-load ${TORQHOME}/code/common/cloudutils.q ${testpath}/settings.q     \
-	-procfile ${testpath}/lprocprocess.csv -debug
+	-procfile ${testpath}/process.csv -debug
 
 #Shut down procs
-${TORQHOME}/torq.sh stop all -csv ${testpath}/lprocprocess.csv
+${TORQHOME}/torq.sh stop all -csv ${testpath}/process.csv
--- tests/cloudlaunchprocess/settings.q
+++ tests/cloudlaunchprocess/settings.q
@@ -1,5 +1,5 @@
 /variables and fns to be used during the unit tests:
-/let the test portno be 7124
+/let the test port_no be 7124
 
 input1:(`procname`proctype`U`localtime`p`T`g`w`qcmd`custom)!("wdb1";"wdb";"${KDBAPPCONFIG}/passwords/accesslist.txt";"0";"7124";"180";"1";"1000";"q";"custom_arg");
 input2:(`procname`proctype)!("wdb1";"wdb");
