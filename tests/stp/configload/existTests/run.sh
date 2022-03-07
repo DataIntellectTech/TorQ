d24b0b9a79ee5192ab0d4f9c175e21b1262747d2
enagle <eoin.nagle@aquaq.co.uk>
2022-03-07 17:27:17 +0000

    removing start/stop all commands

--- tests/stp/configload/existTests/run.sh
+++ tests/stp/configload/existTests/run.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-${TORQHOME}/torq.sh start all
-
 #start test process
 taskset -c 0,1 /usr/bin/rlwrap q ${TORQHOME}/torq.q -proctype segmentedtickerplant -procname stp1 -test ${TORQHOME}/tests/stp/configload/existTests -debug
-
-${TORQHOME}/torq.sh stop all
