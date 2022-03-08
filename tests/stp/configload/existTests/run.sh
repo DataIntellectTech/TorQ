0755c5942f8fcc73e21eff30f35d6190d539af27
enagle <eoin.nagle@aquaq.co.uk>
2022-03-08 12:09:44 +0000

    removing taskset command and replacing with taskset -c 0,1 /usr/bin/rlwrap q in run.sh

--- tests/stp/configload/existTests/run.sh
+++ tests/stp/configload/existTests/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 #start test process
-taskset -c 0,1 /usr/bin/rlwrap q ${TORQHOME}/torq.q -proctype segmentedtickerplant -procname stp1 -test ${TORQHOME}/tests/stp/configload/existTests -debug
+$QCMD ${TORQHOME}/torq.q -proctype segmentedtickerplant -procname stp1 -test ${TORQHOME}/tests/stp/configload/existTests -debug
--- tests/stp/configload/matchTests/run.sh
+++ tests/stp/configload/matchTests/run.sh
@@ -4,4 +4,4 @@
 export KDBAPPCONFIG=${TORQHOME}/tests/stp/configload/matchTests/csvfiles
 
 #start test process
-taskset -c 0,1 /usr/bin/rlwrap q ${TORQHOME}/torq.q -proctype segmentedtickerplant -procname stp1 -test ${TORQHOME}/tests/stp/configload/matchTests -debug
+$QCMD ${TORQHOME}/torq.q -proctype segmentedtickerplant -procname stp1 -test ${TORQHOME}/tests/stp/configload/matchTests -debug
