9bf084b5beb0f2cb2670bc0286327a509aca21a6
CReid <callum.reid@aquaq.co.uk>
2022-03-08 10:56:32 +0000

    Replacing setenv.sh file in stripedvalidid test with smaller setconfig.sh file, adding pause in tests to allow tables to populate

---
+++ tests/striping/stripedvalidid/setconfig.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export TORQAPPHOME=${TORQHOME}/tests/striping/stripedvalidid
+export KDBAPPCONFIG=${TORQAPPHOME}/appconfig
+export KDBAPPCODE=${TORQAPPHOME}/code
+export TORQPROCESSES=${KDBAPPCONFIG}/process.csv
+
