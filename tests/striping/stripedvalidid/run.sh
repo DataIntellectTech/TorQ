d7e4bc69e00300889a665d5bac9dd6204072debc
CReid <callum.reid@aquaq.co.uk>
2022-03-08 13:29:19 +0000

    fixing formatting in run.sh

--- tests/striping/stripedvalidid/run.sh
+++ tests/striping/stripedvalidid/run.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
-export SETENV=$TORQHOME/tests/striping/stripedvalidid/setenv.sh
+# path to test directory
+testpath=$KDBTESTS/striping/stripedvalidid
+SETENV=$testpath/setconfig.sh
 
-${TORQHOME}/torq.sh start discovery1 stp1 feed1
-taskset -c 7,8 /usr/bin/rlwrap q ${TORQHOME}/torq.q -load ${KDBCODE}/processes/rdb.q -proctype rdb -procname rdb1 -segid 1 -test ${KDBTESTS}/striping/stripedvalidid -debug -trap
-${TORQHOME}/torq.sh stop discovery1 stp1 feed1
+# start procs
+${TORQHOME}/torq.sh start discovery1 stp1 feed1 -csv ${testpath}/appconfig/process.csv
+
+# start test proc
+$QCMD ${TORQHOME}/torq.q \
+	-load ${KDBCODE}/processes/rdb.q \
+	-proctype rdb -procname rdb1 \
+	-test ${testpath} \
+	-segid 1 \
+	-trap -debug
+
+# shut down procs
+${TORQHOME}/torq.sh stop all -csv ${testpath}/appconfig/process.csv
