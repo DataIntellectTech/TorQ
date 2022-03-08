550c859525448db5131acb64c0372e21d582535d
AdamBrown2 <adam.brown@aquaq.co.uk>
2022-03-08 14:41:06 +0000

    TCS-20 format change for run.sh

--- tests/stp/subsegment/run.sh
+++ tests/stp/subsegment/run.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-taskset -c 0,1 /usr/bin/rlwrap q ${TORQHOME}/torq.q -proctype segmentedtickerplant -procname stp1 -test ${KDBTESTS}/stp/subsegment/  -procfile ${KDBTESTS}/stp/subsegment/process.csv -schemafile ${TORQAPPHOME}/database.q -load ${KDBCODE}/processes/segmentedtickerplant.q -debug
+$QCMD ${TORQHOME}/torq.q \
+      -proctype segmentedtickerplant \
+      -procname stp1 -test ${KDBTESTS}/stp/subsegment/ \
+      -procfile ${KDBTESTS}/stp/subsegment/process.csv \
+      -schemafile ${TORQAPPHOME}/database.q \
+      -load ${KDBCODE}/processes/segmentedtickerplant.q -debug \
 
