2c10feeb6438efb413124d709d596fc1bc91dc66
CReid <callum.reid@aquaq.co.uk>
2022-03-08 13:48:26 +0000

    changing format of run.sh files for striping tests

--- tests/striping/default/run.sh
+++ tests/striping/default/run.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
-${TORQHOME}/torq.sh start discovery1 stp1 feed1
-taskset -c 7,8 /usr/bin/rlwrap q ${TORQHOME}/torq.q -load ${KDBCODE}/processes/rdb.q -proctype rdb -procname rdb1 -test ${KDBTESTS}/striping/default -debug
-${TORQHOME}/torq.sh stop discovery1 stp1 feed1
+# path to test directory
+testpath=$KDBTESTS/striping/default
+
+# start procs
+${TORQHOME}/torq.sh start discovery1 stp1 feed1 -csv ${testpath}/process.csv
+
+# start test proc
+$QCMD ${TORQHOME}/torq.q \
+	-load ${KDBCODE}/processes/rdb.q \
+	-proctype rdb -procname rdb1 \
+	-test ${testpath} \
+	-trap -debug
+
+# shut down procs
+${TORQHOME}/torq.sh stop all -csv ${testpath}/process.csv
