4a5df4fdba9869cc8d13af5722cfca6f5e6d6106
CReid <callum.reid@aquaq.co.uk>
2022-03-29 13:25:50 +0100

    Modification to test script to load test process.csv file

--- tests/tailer/run.sh
+++ tests/tailer/run.sh
@@ -4,13 +4,14 @@
 export KDBAPPCONFIG=${TORQHOME}/tests/tailer/config
 
 # start procs
-${TORQHOME}/torq.sh start discovery1 stp1 feed1
+${TORQHOME}/torq.sh start discovery1 stp1 feed1 -csv ${KDBAPPCONFIG}/process.csv
 
 # start test process
 $QCMD ${TORQHOME}/torq.q \
 	-load ${KDBCODE}/processes/rdb.q \
 	-proctype rdb -procname rdb1 \
+	-csv ${KDBAPPCONFIG}/process.csv \
 	-test ${TORQHOME}/tests/tailer -debug
 
 # shut down procs
-${TORQHOME}/torq.sh stop all -csv $TORQPROCESSES
+${TORQHOME}/torq.sh stop all -csv ${KDBAPPCONFIG}/process.csv
