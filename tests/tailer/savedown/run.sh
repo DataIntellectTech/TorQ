7822a53cd0ba29be7d9e0261569bcd5aa654fe98
jdhalligan <jhalligan@homer.aquaq.co.uk>
2022-03-31 11:00:13 +0100

    adding unit tests

---
+++ tests/tailer/savedown/run.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Path to test directory
+testpath=${KDBTESTS}/tailer/savedown
+
+# Start procs
+${TORQHOME}/torq.sh start discovery1 -procfile ${testpath}/process.csv
+
+
+# Start test proc
+#taskset -c 0,1 /usr/bin/rlwrap q ${TORQHOME}/torq.q \
+#  -proctype test -procname test1 \
+#  -test ${testpath} \
+#  -load ${KDBTESTS}/helperfunctions.q ${testpath}/settings.q \
+#  -testresults ${KDBTESTS}/tailer/savedown \
+#  -runtime $run \
+#  -procfile ${testpath}/process.csv \
+#  $debug $stop $write $quiet
+
+$QCMD ${TORQHOME}/torq.q \
+      -proctype wdb \
+      -procname wdb1 -test ${KDBTESTS}/tailer/savedown/ \
+      -procfile ${KDBTESTS}/tailer/savedown/process.csv \
+      -load ${KDBCODE}/processes/wdb.q -debug \
---
+++ tests/tailer/savedown/settings.q
@@ -0,0 +1,7 @@
+// Paths to process CSV and temp HDB directory
+processcsv:getenv[`KDBTESTS],"/tailer/process.csv";
+temphdbdir:hsym `$getenv[`KDBTESTS],"/tailer/tmphdb/";
+
+// Test tables
+testtrade:([].z.p - 01:00+til 50;sym:50?`IBM`GOOG`MSFT`AAPL;price:50?100.00;size:50?150;stop:50?0 1; cond:50?.Q.A;ex:50?`O`N;size:50?`buy`sell)
+testquote:([]time:.z.p-01:00+til 50;sym:50?`AAPL`MSFT`GOOG`IBM;bid:50?500.0;ask:50?500.0;bsize:50?200;asize:50?200;mode:50?.Q.A;ex:50?`N`O;src:50?`BARX`DB`GETGO`SUN)
