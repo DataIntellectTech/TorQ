4bd78e3d76ba3470530fb54060abd418aa076bf5
jdhalligan <jhalligan@homer.aquaq.co.uk>
2022-04-04 12:32:08 +0100

    added unit tests

--- tests/tailer/savedown/run.sh
+++ tests/tailer/savedown/run.sh
@@ -4,21 +4,23 @@
 testpath=${KDBTESTS}/tailer/savedown
 
 # Start procs
-${TORQHOME}/torq.sh start discovery1 -procfile ${testpath}/process.csv
+${TORQHOME}/../devTorQCloud/torq.sh start all -procfile ${testpath}/process.csv
 
 
 # Start test proc
-#taskset -c 0,1 /usr/bin/rlwrap q ${TORQHOME}/torq.q \
-#  -proctype test -procname test1 \
-#  -test ${testpath} \
-#  -load ${KDBTESTS}/helperfunctions.q ${testpath}/settings.q \
-#  -testresults ${KDBTESTS}/tailer/savedown \
-#  -runtime $run \
-#  -procfile ${testpath}/process.csv \
-#  $debug $stop $write $quiet
-
 $QCMD ${TORQHOME}/torq.q \
-      -proctype wdb \
-      -procname wdb1 -test ${KDBTESTS}/tailer/savedown/ \
-      -procfile ${KDBTESTS}/tailer/savedown/process.csv \
-      -load ${KDBCODE}/processes/wdb.q -debug \
+  -proctype wdb -procname test1 \
+  -test ${testpath} \
+  -load ${KDBTESTS}/helperfunctions.q ${testpath}/settings.q \
+  -testresults ${KDBTESTS}/tailer/savedown/ \
+  -runtime $run \
+  -procfile ${testpath}/process.csv \
+  $debug $stop $write $quiet
+
+#$QCMD ${TORQHOME}/torq.q \
+#      -proctype wdb \
+#      -procname wdb1 -test ${KDBTESTS}/tailer/savedown/ \
+#      -procfile ${KDBTESTS}/tailer/savedown/process.csv \
+#      -load ${KDBCODE}/processes/wdb.q -debug \
+
+${TORQHOME}/../devTorQCloud/torq.sh stop all -procfile ${testpath}/process.csv
