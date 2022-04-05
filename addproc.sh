40eda8fe0fc32379f559eb7d86a3b410bbadf549
MateaKanjuga <matea.kanjuga@aquaq.co.uk>
2022-04-05 19:04:13 +0100

    Remove unnecessary line, add back -csv flag

--- addproc.sh
+++ addproc.sh
@@ -19,7 +19,6 @@ suffix=$2
 
 #work out new procname based on second and third arg provided
 newprocname="$inputprocname"."$suffix"
-newprocname1="$inputprocname"\."$suffix"
 
 #determine port number increment based on number of lines in the process.csv
 #if that port is already in use, increment by 1 till succesful
@@ -52,6 +51,6 @@ else
 #run summary of all processes in customprocess.csv
 	grep $inputprocname ${KDBAPPCONFIG}/process.csv | awk -F',' -vOFS=',' '{ $2 = "{KDBBASEPORT}+" '"$portnum"'; $4 ="'$newprocname'" }1' >> ${KDBAPPCONFIG}/customprocess.csv
     	echo "$inputprocname replicated as $newprocname" 
-	bash torq.sh start $newprocname 
-	bash torq.sh summary
+	bash torq.sh start $newprocname -csv ${KDBAPPCONFIG}/customprocess.csv
+	bash torq.sh summary -csv ${KDBAPPCONFIG}/customprocess.csv
 fi
