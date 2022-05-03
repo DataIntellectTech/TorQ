b1ce74da6dc57ab6c744dd2a838bc8b5ea549998
eointeague <eoin.teague@aquaq.co.uk>
2022-05-03 14:51:33 +0100

    tidy up rdb run.sh file

--- tests/rdb/runrdbtests.sh
+++ tests/rdb/runrdbtests.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-./torq.sh start discovery1
-./torq.sh start stp1
-./torq.sh start feed1
-/usr/bin/rlwrap q torq.q -load ${KDBCODE}/processes/rdb.q -proctype rdb -procname rdb1 -test ${KDBTESTS}/rdb -debug
-./torq.sh stop all
+# Start procs
+${TORQHOME}/torq.sh start discovery1
+${TORQHOME}/torq.sh start stp1
+${TORQHOME}/torq.sh start feed1
+
+# Start test proc
+/usr/bin/rlwrap q ${TORQHOME}/torq.q -load ${KDBCODE}/processes/rdb.q -proctype rdb -procname rdb1 -test ${KDBTESTS}/rdb -debug
+
+# Shut down procs
+${TORQHOME}/torq.sh stop discovery1 stp1 feed1
