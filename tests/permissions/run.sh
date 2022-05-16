162434dc1b5f2a6462b1b0b191c0811cd0953872
eointeague <eoin.teague@aquaq.co.uk>
2022-05-16 16:48:47 +0100

    handle command line args for permissions test run.sh

--- tests/permissions/run.sh
+++ tests/permissions/run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Handle command-line arguments
+source $KDBTESTS/flagparse.sh
+
 # Path to test directory
 testpath=${KDBTESTS}/permissions
 
@@ -8,4 +11,4 @@ ${RLWRAP} ${QCMD} ${TORQHOME}/torq.q \
   -proctype test -procname test1 \
   -test ${testpath} \
   -load ${KDBTESTS}/helperfunctions.q  \
-  -debug
+  $debug $stop $write $quiet 
