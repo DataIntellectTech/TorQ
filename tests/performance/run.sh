b332b514b0968a6021c12a4a8e1594bfcc4798d7
eointeague <eoin.teague@aquaq.co.uk>
2022-05-19 14:13:30 +0100

    update performance tests to use QCMD

--- tests/performance/run.sh
+++ tests/performance/run.sh
@@ -7,7 +7,7 @@ localpath=$KDBTESTS/performance
 ${TORQHOME}/torq.sh start discovery1 feed1 stp1 tp1 consumer1 tick1 -csv ${localpath}/settings/process.csv
 
 # Start test proc
-/usr/bin/rlwrap q ${TORQHOME}/torq.q \
+${RLWRAP} ${QCMD} ${TORQHOME}/torq.q \
   -proctype observer -procname observer1 \
   -load ${localpath}/settings/observer.q ${localpath}/code/observer.q \
   -procfile ${localpath}/settings/process.csv \
