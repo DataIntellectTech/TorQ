a7e9baee92fd4c7ed1e8b3933db0261e51a20214
Ethan Sweeney <ethan.sweeney@aquaq.co.uk>
2022-09-06 10:01:39 +0100

    remove commented lines

--- tests/datastore/run.sh
+++ tests/datastore/run.sh
@@ -19,10 +19,4 @@ $QCMD ${TORQHOME}/torq.q \
   -segid 1 \
   $debug $stop $write $quiet
 
-#$QCMD ${TORQHOME}/torq.q \
-#      -proctype wdb \
-#      -procname wdb1 -test ${KDBTESTS}/tailer/savedown/ \
-#      -procfile ${KDBTESTS}/tailer/savedown/process.csv \
-#      -load ${KDBCODE}/processes/wdb.q -debug \
-
 ${TORQHOME}/../deploy/torq.sh stop all -procfile ${testpath}/process.csv
