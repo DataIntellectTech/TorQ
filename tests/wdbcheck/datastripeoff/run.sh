3a243a53809e1bb2ad1135c55d44966c92b2b228
ShawnTay96 <shawn.tay@aquaq.co.uk>
2022-10-04 12:10:47 +0100

    Add unit tests

---
+++ tests/wdbcheck/datastripeoff/run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+source $KDBTESTS/flagparse.sh
+
+# Path to test directory
+testpath=${KDBTESTS}/wdbcheck/datastripeoff
+
+${TORQHOME}/../devTorQCloud/torq.sh start all -procfile ${testpath}/process.csv
+
+# Start test proc
+$QCMD ${TORQHOME}/torq.q \
+  -proctype wdb -procname wdb1 \
+  -procfile ${testpath}/process.csv \
+  -test ${testpath} \
+  -load ${KDBTESTS}/helperfunctions.q \
+  -runtime $run \
+  -testresults ${testpath}/results \
+  -debug
+
+${TORQHOME}/../devTorQCloud/torq.sh stop all -procfile ${testpath}/process.csv
