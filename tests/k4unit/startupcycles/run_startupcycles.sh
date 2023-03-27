e802fb10ea0ec46ab614bcbca6cd1b5bdf0749e4
emma.email win@aquaq.co.uk <egoodwin@homer.aquaq.co.uk>
2023-03-27 11:18:53 +0100

    #561 Errors with string now and added unit test

---
+++ tests/k4unit/startupcycles/run_startupcycles.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+${TORQHOME}/torq.sh start discovery1
+${TORQHOME}/torq.sh start stp1
+${TORQHOME}/torq.sh start feed1
+
+#own
+${QCMD} ${TORQHOME}/torq.q \
+ -proctype rdb -procname rdb1 \
+ -test ${KDBTESTS}/k4unit/startupcycles/ \
+ -load ${KDBCODE}/processes/rdb.q  \
+ -testresults ${KDBTESTS}/k4unit/logs/ \
+ -runtime $run \
+ $debug $stop $write $quiet
+
+${TORQHOME}/torq.sh stop all
