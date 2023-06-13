b869de00710b32dd3a71d942dfebb230aef3f023
Andrew Steele <andrew.steele@aquaq.co.uk>
2023-06-13 15:52:21 +0100

    Fix up pcap tests
    Use full paths in run script
    Add a process.csv
    define the filealerter function
    add schema for tickerplant and rdb

--- tests/pcap/runpcaptests.sh
+++ tests/pcap/runpcaptests.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
-./torq.sh start discovery1
-./torq.sh start tickerplant1 
-./torq.sh start rdb1 -extras -.rdb.replaylog 0
-rlwrap q torq.q -load ${KDBCODE}/processes/filealerter.q -proctype filealerter -procname filealerter1 -test ${KDBTESTS}/pcap -debug -.fa.polltime 00:10 -.servers.CONNECTIONS -.fa.tickerplanttype -.fa.alreadyprocessed ${KDBTESTS}/pcap/filealerterprocessed -.fa.inputcsv ${KDBTESTS}/pcap/dummyfilealerter.csv
-./torq.sh stop all
+. ${KDBTESTS}/flagparse.sh
+
+ALREADYPROCESSED=${KDBTESTS}/pcap/settings/filealerterprocessed
+
+${TORQHOME}/torq.sh start discovery1 -csv ${KDBTESTS}/pcap/settings/process.csv
+${TORQHOME}/torq.sh start tickerplant1 -csv ${KDBTESTS}/pcap/settings/process.csv
+${TORQHOME}/torq.sh start rdb1 -csv ${KDBTESTS}/pcap/settings/process.csv -extras -.rdb.replaylog 0
+${RLWRAP} ${QCMD} ${TORQHOME}/torq.q -load ${KDBCODE}/processes/filealerter.q \
+    -proctype filealerter -procname filealerter1 \
+    -procfile ${KDBTESTS}/pcap/settings/process.csv \
+    -test ${KDBTESTS}/pcap ${debug} \
+    -.fa.polltime 00:10  \
+    -.fa.inputcsv ${KDBTESTS}/pcap/settings/dummyfilealerter.csv \
+    -.fa.alreadyprocessed ${ALREADYPROCESSED} \
+    -.fa.tickerplanttypes tickerplant
+
+RC=$?
+${TORQHOME}/torq.sh stop all -csv ${KDBTESTS}/pcap/settings/process.csv
+rm -rf ${ALREADYPROCESSED}
+
+exit $RC
---
+++ tests/pcap/settings/schema.q
@@ -0,0 +1,2 @@
+quote:([]time:`timestamp$(); sym:`$(); bid:"f"$(); ask:"f"$(); bsize:"j"$(); asize:"j"$(); mode:"c"$(); ex:"c"$(); src:`$());
+packets:([]time:`timestamp$(); sym:`$(); src:`$(); dest:`$(); srcport:"j"$(); destport:"j"$(); seq:"j"$(); ack:"j"$(); win:"j"$(); tsval:"j"$(); tsecr:"j"$(); flags:(); protocol:`$(); length:"j"$(); len:"j"$(); data:());
